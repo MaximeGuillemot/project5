9f312c497db6aee09dde0df199c6b1b4e729e885
Maxime Guillemot <guillemot.maxime@gmail.com>
2018-12-05 22:30:36 +0100

    First basic HTML file

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+
+		<title>MMOShards</title>
+
+		<meta name="description" content="Actualités sur les MMORPG" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="application-name" content="MMOShards" />
+		
+		<meta property="og:description" content="Actualités sur les MMORPG" />
+		<meta property="og:title" content="MMOShards - Vos actus MMO" />
+		<meta property="og:site_name" content="MMOShards" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://maximeguillemot.com/formation/project5/" />
+		<meta property="og:image" content="" />
+		
+		<meta name="twitter:description" content="Actualités sur les MMORPG" />
+		<meta name="twitter:card" content="MMOShards - Vos actus MMO" />
+		<meta name="twitter:site" content="@MMOShards" />
+		<meta name="twitter:title" content="MMOShards - Vos actus MMO" />
+		<meta name="twitter:image" content="" />
+
+		<link rel="icon" sizes="16x16" href="images/favicon.ico" />
+
+		<link href="styles/styles.css" type="text/css" rel="stylesheet" media="all" />
+	</head>
+
+	<body>
+		<header>
+			<h1>MMOShards</h1>
+		</header>
+
+		<nav>
+
+        </nav>
+
+        <main>
+
+        </main>
+		
+		<footer>
+            <p><small>©2018 Tous droits réservés @MMOShards.</small></p>
+		</footer>
+	</body>
+</html>
